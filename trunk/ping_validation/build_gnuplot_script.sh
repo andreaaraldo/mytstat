c1c8d6ec02434c554a1a4f74250aa4ecc7f5fbae
andrea.araldo@gmail.com <andrea.araldo@gmail.com@7197c468-ff3e-591c-f3eb-fe98a9c9ed49>
2013-04-11 16:15:24 +0000

    before optimization study

--- trunk/ping_validation/build_gnuplot_script.sh
+++ trunk/ping_validation/build_gnuplot_script.sh
@@ -14,10 +14,16 @@ SAMPLE_ACK_TRIG_FILE=$LAST_RUN_FOLDER/log_tcp_qd_sample_acktrig
 #to print both graphs
 echo -ne "reset;\n" > $PING_SCRIPT
 echo -ne "set grid; show grid; set xlab 'timestamp';\n" >> $PING_SCRIPT
+echo -ne "set format x \"%10.0f\";" >> $PING_SCRIPT
 echo -ne "set ylab '[ms]'; set ytics nomirror; \n" >> $PING_SCRIPT
+echo -ne "set tmargin 0; set bmargin 0; set lmargin 10; set rmargin 10;" >> $PING_SCRIPT
 
-#I want my graphs be placesd in 2 rows and 1 column.
-echo -ne "set multiplot layout 2,1;\n" >> $PING_SCRIPT
+#I want my graphs be placesd in 2 rows and 1 column. I ask for 3 plots (although
+#I need only 2 plots) in order to gain space
+#See gnuplot.sourceforge.net/demo/layout.html
+echo -ne "set multiplot layout 3,1;\n" >> $PING_SCRIPT
+
+echo -ne "unset xtics;" >> $PING_SCRIPT
 echo -ne "plot " >> $PING_SCRIPT
 
 #echo -ne "'< cat $WIN_ACK_TRIG_FILE' u 1:(\$11) with linespoints axes x1y1 title 'data2ack_C2S' " >> $PING_SCRIPT
@@ -28,8 +34,10 @@ echo -ne "'< cat $WIN_ACK_TRIG_FILE' u 1:(\$7) with linespoints axes x1y1 title 
 echo -ne ", '< cat $WIN_ACK_TRIG_FILE' u 1:(\$20) with linespoints axes x1y1 title 'ack_trig_windowed_qd_S2C'" >> $PING_SCRIPT
 
 echo -ne ", '< cut -d= -f4- /tmp/ping.DATA' u 1:3 with linespoints axes x1y1 title 'rtt_ping';\n" >> $PING_SCRIPT
+echo -ne "set xrange [GPVAL_X_MIN:GPVAL_X_MAX];\n" >> $PING_SCRIPT
 
 echo -ne "set ylab 'validity ratio'; set y2lab 'no samples/window'; set ytics nomirror; set y2tics;\n" >> $PING_SCRIPT
+echo -ne "set xtics nomirror rotate by -90;\n" >> $PING_SCRIPT
 
 echo -ne "plot " >> $PING_SCRIPT
 
@@ -41,5 +49,8 @@ echo -ne "'< cat $WIN_ACK_TRIG_FILE' u 1:(\$26) with linespoints axes x1y2 title
 #echo -ne ", '< cat $WIN_ACK_TRIG_FILE' u 1:($13/\$10) with linespoints axes x1y1 title 'validity_ratio_C2S'" >> $PING_SCRIPT
 
 echo -ne ", \"< awk '{if(\$23>0) print \$1,\$26/\$23 }' $WIN_ACK_TRIG_FILE\" u 1:2 with linespoints axes x1y1 title 'validity_ratio_S2C';\n" >> $PING_SCRIPT
+
+#To be sure that the 2 graphs have exactly the same ticks
+echo -ne "set xrange [GPVAL_X_MIN:GPVAL_X_MAX];\n" >> $PING_SCRIPT
 
 echo -ne "pause 2; reread;\n" >> $PING_SCRIPT
