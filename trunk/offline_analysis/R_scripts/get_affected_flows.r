ab5b1df0bb2f91aadca3da8c6f6e8360a14b71e6
andrea.araldo@gmail.com <andrea.araldo@gmail.com@7197c468-ff3e-591c-f3eb-fe98a9c9ed49>
2013-05-06 14:01:54 +0000

    global analysis of fastweb2

--- trunk/offline_analysis/R_scripts/get_affected_flows.r
+++ trunk/offline_analysis/R_scripts/get_affected_flows.r
@@ -1,11 +1,20 @@
 library(plyr)
 
 
+####### CONSTANTS
+percentiles_cdf_plot <- "/home/araldo/temp/percentiles_cdf.jpg"
+qd_pdf_plot <- "/home/araldo/temp/qd_pdf.jpg"
+percentiles_savefile <- "/home/araldo/temp/percentiles.R.save"
+qd_of_flows_to_study_savefile <- 
+    "/home/araldo/temp/qd_flows_to_study.R.save"
+chosen_qd_threshold <- 0
+chosen_flow_length_threshold <- 0
+
 
 # We will consider only flows in which we observe at least flow_length_threshold
 # non  empty windows and that have experienced at least one time a 
 # windowed_qd >= qd_threshold
-process_windows <- function(dataframe, qd_threshold, 
+process_windows <- function(windows, qd_threshold, 
                             flow_length_threshold)
 {
     flow_id_vector <- c("ipaddr1","port1","ipaddr2","port2")
@@ -44,7 +53,7 @@ process_windows <- function(dataframe, qd_threshold,
         ]
     
     
-    ####### affected_flows
+    ####### flows_to_study
     
     # Take all the windows with a windowed_qd_C2S >= qd_threshold
     suspected_windows_C2S_ <- windows[
@@ -67,38 +76,38 @@ process_windows <- function(dataframe, qd_threshold,
     # The following is like an SQL inner join (from the table on the left
     # I want to preserve only the rows that have a corrispondence with 
     # some rows of the table on the right)
-    affected_flows_C2S_ <- merge(suspected_windows_C2S_, long_flows_C2S)
-    affected_flows_S2C_ <- merge(suspected_windows_S2C_, long_flows_S2C)
+    flows_to_study_C2S_ <- merge(suspected_windows_C2S_, long_flows_C2S)
+    flows_to_study_S2C_ <- merge(suspected_windows_S2C_, long_flows_S2C)
     
     #Purge duplicated flows
-    affected_flows_C2S <- 
-        affected_flows_C2S_[ 
-            !duplicated( affected_flows_C2S_[,flow_id_vector]), 
+    flows_to_study_C2S <- 
+        flows_to_study_C2S_[ 
+            !duplicated( flows_to_study_C2S_[,flow_id_vector]), 
             ]
     
-    affected_flows_S2C <- 
-        affected_flows_S2C_[ 
-            !duplicated( affected_flows_S2C_[,flow_id_vector]), 
+    flows_to_study_S2C <- 
+        flows_to_study_S2C_[ 
+            !duplicated( flows_to_study_S2C_[,flow_id_vector]), 
             ]
     
     
     ####### Ratios
     affected_ratio_over_all_flows <- 
-        ( length(affected_flows_C2S[,1]) + 
-              length(affected_flows_S2C[,1]) ) /
+        ( length(flows_to_study_C2S[,1]) + 
+              length(flows_to_study_S2C[,1]) ) /
         ( length(all_non_empty_flows_C2S[,1]) + 
               length(all_non_empty_flows_S2C[,1]) 
         )
     
     affected_ratio_over_long_enough_flows <- 
-        (   length(affected_flows_C2S[,1]) + 
-                length(affected_flows_S2C[,1]) ) /
+        (   length(flows_to_study_C2S[,1]) + 
+                length(flows_to_study_S2C[,1]) ) /
         (   length(long_flows_C2S[,1]) + 
                 length(long_flows_S2C[,1]) 
         )
     
-    length(affected_flows_C2S[,1])
-    length(affected_flows_S2C[,1])
+    length(flows_to_study_C2S[,1])
+    length(flows_to_study_S2C[,1])
     length(long_flows_C2S[,1])
     length(long_flows_S2C[,1])
     length(all_non_empty_flows_C2S[,1])
@@ -106,23 +115,50 @@ process_windows <- function(dataframe, qd_threshold,
     ####### windowed qd of the affected flows
     
     # Take the windows of the affected flows
-    qd_of_affected_flows_C2S_ <- merge(windows, affected_flows_C2S)
-    qd_of_affected_flows_S2C_ <- merge(windows, affected_flows_S2C)
+    windows_to_study_C2S_ <- merge(windows, flows_to_study_C2S)
+    windows_to_study_S2C_ <- merge(windows, flows_to_study_S2C)
     
     # Consider only the non empty windows
-    qd_of_affected_flows_C2S <- 
-        qd_of_affected_flows_C2S_[ 
-            qd_of_affected_flows_C2S_$qd_samples_C2S>0, "windowed_qd_C2S"
-            ]
+    windows_to_study_C2S <- 
+        windows_to_study_C2S_[
+            windows_to_study_C2S_$qd_samples_C2S>0,]
     
-    qd_of_affected_flows_S2C <- 
-        qd_of_affected_flows_S2C_[ 
-            qd_of_affected_flows_S2C_$qd_samples_S2C>0, "windowed_qd_S2C"
-            ]
+    windows_to_study_S2C <- 
+        windows_to_study_S2C_[
+            windows_to_study_S2C_$qd_samples_S2C>0,]
+    
+    qd_of_flows_to_study_C2S <- 
+        windows_to_study_C2S[, "windowed_qd_C2S"]
+    
+    qd_of_flows_to_study_S2C <- 
+        windows_to_study_S2C[, "windowed_qd_S2C"]
     
     # Unify C2S and S2C directions
-    qd_of_affected_flows <-
-        c(qd_of_affected_flows_C2S, qd_of_affected_flows_S2C)
+    qd_of_flows_to_study <-
+        c(qd_of_flows_to_study_C2S, qd_of_flows_to_study_S2C)
+    
+    ####### Per flow percentiles
+    percentiles_df_C2S <- 
+        ddply(windows_to_study_C2S, .(ipaddr1, port1, ipaddr2, port2), 
+              summarize, 
+              percentile_90 = quantile( windowed_qd_C2S, c(.90)),
+              percentile_95 = quantile( windowed_qd_C2S, c(.95)),
+              percentile_99 = quantile( windowed_qd_C2S, c(.99))
+        )
+    
+    percentiles_df_S2C <- 
+        ddply(windows_to_study_S2C, .(ipaddr1, port1, ipaddr2, port2), 
+              summarize, 
+              percentile_90 = quantile( windowed_qd_S2C, c(.90)), 
+              percentile_95 = quantile( windowed_qd_S2C, c(.95)), 
+              percentile_99 = quantile( windowed_qd_S2C, c(.99))
+        )
+
+    
+    # Unify percentiles
+    percentile_names <- c("percentile_90","percentile_95","percentile_99")
+    percentiles <- rbind( percentiles_df_C2S[,percentile_names],
+                          percentiles_df_S2C[,percentile_names])
     
     
     #######SEVERE_DEBUG: begin
@@ -130,8 +166,8 @@ process_windows <- function(dataframe, qd_threshold,
     # For we have taken only the windows with qd_samples>0, in all windows
     # should be possible to compute the windowed qd. Therefore, NA value
     # are impossibile
-    if( length( qd_of_affected_flows[is.na(qd_of_affected_flows)]) != 0)
-        stop("ERROR: NA value are forbidden in qd_of_affected_flows")
+    if( length( qd_of_flows_to_study[is.na(qd_of_flows_to_study)]) != 0)
+        stop("ERROR: NA value are forbidden in qd_of_flows_to_study")
     
     if(
         length (
@@ -146,49 +182,117 @@ process_windows <- function(dataframe, qd_threshold,
     
     #SEVERE_DEBUG: end
     
+    # Sarebbe bello printarli
+    qd_threshold
+    flow_length_threshold
+    affected_ratio_over_all_flows
+    affected_ratio_over_long_enough_flows
+    length(qd_of_flows_to_study)
+    quantile(qd_of_flows_to_study,c(.90,.95,.99))
     
-    print(qd_threshold)
-    print(flow_length_threshold)
-    print(affected_ratio_over_all_flows)
-    print(affected_ratio_over_long_enough_flows)
-    print(length(qd_of_affected_flows) )
-    print(quantile(qd_of_affected_flows,c(.90,.95,.99)) )
-    
-    bins <- seq(min(qd_of_affected_flows), 
-                max(qd_of_affected_flows)+10,10)
-    
-    zoom <- c(0,1000)
-    hist(qd_of_affected_flows, breaks=bins, xlim=zoom, 
-         plot=TRUE)
-    
-    stop("questa e' prova")
+    return(list(qd_of_flows_to_study=qd_of_flows_to_study, 
+                percentiles=percentiles) )
 }
 
-field_names<-c("edge", "ipaddr1","port1","ipaddr2","port2",
-               "type_C2S",
-               "windowed_qd_C2S","error_C2S","max_qd_C2S",
-               "chances_C2S",
-               "grossdelay_C2S","conn_id_C2S","qd_samples_C2S",
-               "not_void_windows_C2S","sample_qd_sum_C2S",
-               "window_qd_sum_C2S",
-               "sample_qd_sum_until_last_window_C2S",
-               "delay_base_C2S",
-               
-               "type_S2C",
-               "windowed_qd_S2C","error_S2C","max_qd_S2C",
-               "chances_S2C",
-               "grossdelay_S2C","conn_id_S2C","qd_samples_S2C",
-               "not_void_windows_S2C","sample_qd_sum_S2C",
-               "window_qd_sum_S2C",
-               "sample_qd_sum_until_last_window_S2C",
-               "delay_base_S2C")
+plot_percentiles <- function(percentiles)
+{
+    max_x <- max(c(max(percentiles[,c("percentile_90")]),
+                   max(percentiles[,c("percentile_95")]),
+                   max(percentiles[,c("percentile_99")]) )
+    )
+    jpeg(percentiles_cdf_plot)
+    
+    plot(ecdf(percentiles[,c("percentile_90")]), 
+         log="x", xlim=c(1,max_x))
+    
+    plot(ecdf(percentiles[,c("percentile_95")]), 
+         log="x", xlim=c(1,max_x), add=TRUE )
+    
+    plot(ecdf(percentiles[,c("percentile_99")]), 
+         log="x", xlim=c(1,max_x), add=TRUE )
+    
+    dev.off()
+}
 
-windows <- read.table("/home/araldo/analysis_outputs/14_56_21_Jun.dump01.gz/2007_06_21_14_56.out/log_tcp_windowed_qd_acktrig",
-                      sep=" ", 
-                      col.names=field_names, 
-                      na.strings = "-",
-                      fill=FALSE, 
-                      strip.white=TRUE)
+plot_qd <- function(qd_of_flows_to_study)
+{
+    jpeg(qd_pdf_plot)
+    bins <- seq(min(qd_of_flows_to_study), 
+                max(qd_of_flows_to_study)+10,10)
+    
+    zoom <- c(0,1000)
+    hist(qd_of_flows_to_study, breaks=bins, xlim=zoom, 
+             plot=TRUE)
+    dev.off()
+}
+
+load_window_log <- function(filename)
+{
+    field_names<-c("edge", "ipaddr1","port1","ipaddr2","port2",
+                   "type_C2S",
+                   "windowed_qd_C2S","error_C2S","max_qd_C2S",
+                   "chances_C2S",
+                   "grossdelay_C2S","conn_id_C2S","qd_samples_C2S",
+                   "not_void_windows_C2S","sample_qd_sum_C2S",
+                   "window_qd_sum_C2S",
+                   "sample_qd_sum_until_last_window_C2S",
+                   "delay_base_C2S",
+                   
+                   "type_S2C",
+                   "windowed_qd_S2C","error_S2C","max_qd_S2C",
+                   "chances_S2C",
+                   "grossdelay_S2C","conn_id_S2C","qd_samples_S2C",
+                   "not_void_windows_S2C","sample_qd_sum_S2C",
+                   "window_qd_sum_S2C",
+                   "sample_qd_sum_until_last_window_S2C",
+                   "delay_base_S2C")
+    
+    windows <- read.table(filename,
+                           sep=" ", 
+                           col.names=field_names, 
+                           na.strings = "-",
+                           fill=FALSE, 
+                           strip.white=TRUE)
+
+    return(windows)
+}
+
+process_logfile <- function(filename)
+{
+    windows_ <-load_window_log(filename)
+    
+    result <- process_windows(
+        windows=windows_[,], 
+        qd_threshold=chosen_qd_threshold, 
+        flow_length_threshold=flow_length_threshold)
+
+    return(result)
+}
+
+filelist <- list.files(path = "/home/araldo/analysis_outputs/", 
+           pattern = "*log_tcp_windowed_qd_acktrig", all.files = FALSE,
+           full.names = TRUE, recursive = TRUE,
+           ignore.case = FALSE, include.dirs = FALSE)
+
+result <- process_logfile(filelist[1])
+percentiles <- result$percentiles
+qd_of_flows_to_study <- result$qd_of_flows_to_study
+
+for (filename in filelist[2:length(filelist)])
+{
+    print("Processing another file")
+    result <- process_logfile(filename)
+    percentiles <- rbind(percentiles, result$percentiles)
+    
+    qd_of_flows_to_study <- 
+        c(qd_of_flows_to_study, result$qd_of_flows_to_study)
+}
+
+save(percentiles, file=percentiles_savefile)
+save(qd_of_flows_to_study, file=qd_of_flows_to_study_savefile)
 
 
-process_windows(windows, 0, 0)
+load(percentiles_savefile)
+plot_percentiles(percentiles)
+load(qd_of_flows_to_study_savefile)
+plot_qd(qd_of_flows_to_study)
